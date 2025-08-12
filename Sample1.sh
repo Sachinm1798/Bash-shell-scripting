aeb5f5f6013b5b35e02bd1623ab118720c6887f4
sachin <sachinswatz1798@gmail.com>
2025-08-12 10:43:14 +0530

    Sample scripts in linux

---
+++ Sample1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+##########################
+#User: Sachin M
+#Date :12/08/2025
+#This script is to get output of Node Health
+#Version:1
+###########################
+ echo "This the disk space in the system"
+ df
+ echo "This the Memory inFormation"
+ free -g
+echo "This is the number of processors"
+nproc
